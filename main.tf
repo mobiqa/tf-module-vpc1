ee843b84337cb61a9471618b16ea5cf7030f93ab
venkat <venkat.mobiqa@gmail.com>
2023-03-31 10:08:42 +0530

    updating ./tf-module-vpc1

--- main.tf
+++ main.tf
@@ -87,7 +87,7 @@ resource "aws_instance" "web" {
   ami                    = data.aws_ami.centos8.id
   instance_type          = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  subnet_id              =  aws_subnet.main.*.id[0]
+//  subnet_id              =  aws_subnet.main.*.id[0]
 
 
   tags = {
