a454c7bf39ff0a915ab91c5c37d8c13e30ea02f3
venkat <venkat.mobiqa@gmail.com>
2023-05-12 08:38:52 +0530

    updating ./tf-module-vpc1

--- locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  common_tags = {
-    env           = var.env
-    project       = "roboshop"
-    business_unit = "ecommerce"
-    owner         = "ecommerce-robot"
-  }
-}
--- vars.tf
+++
@@ -1,6 +0,0 @@
-variable "cidr_block" {}
-variable "env" {}
-variable "default_vpc_id" {}
-variable "public_subnets" {}
-variable "private_subnets" {}
-variable "availability_zone" {}
